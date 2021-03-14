2f2d58f30b87ef8aa664f8291fe15b2b1686dd8d
karenmentges <karen_ruvermentges@hotmail.com>
2021-03-14 00:05:42 -0300

    Changes

---
+++ Trabalho 1/BubbleSortFlag.c
@@ -0,0 +1,16 @@
+Algoritmo Bubble com Flag
+Início
+ para i de n-1 até 1 faça
+ flag = falso
+ para j de 0 até i-1 faça
+ se A[j] > a[j+1] então
+ troca(A[j], A[j+1])
+ flag = verdadeiro
+ fimSe
+ fimPara
+ se flag = falso então
+ interrompa
+ fimSe
+ fimPara
+fimAlgoritmo
+

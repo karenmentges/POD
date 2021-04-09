ce8778fabd9afc33b2ffbd6a0a119479973e5c96
karenmentges <karen_ruvermentges@hotmail.com>
2021-04-08 22:29:06 -0300

    Changes

--- Manipulação_de_Arquivos_em_C/Atividade_1.c
+++ Manipulação_de_Arquivos_em_C/Atividade_1.c
@@ -4,7 +4,9 @@
 
 int main() {
 
+    char c;
     char texto[229];
+    int i;
 
     // Leitura do texto
     FILE *arq = fopen("Poem.txt", "r");
@@ -12,20 +14,44 @@ int main() {
         printf("Não foi possível acessar o arquivo!");
         exit(1);
     }
+    i = 0;
+    while(!feof(arq)) {
+        c = fgetc(arq);
+        //printf("%c", c);
+        if(c=='\n' || c==' '){
+            //printf("%c", c);
+            texto[i] = c;
+            i++;
+        }
+        else{
+            if(c<=90){
+                //printf("%c", c);
+                texto[i] = c;
+                i++;
+            }
+            else{
+                c = c - 32;
+                //printf("%c", c);
+                texto[i] = c;
+                i++;
+            }
+        }  
+    }   
+    fclose(arq);   
     
-    fgets(texto, 229, arq);
-
-    fclose(arq);
-    
-
+    // Gravação do texto em outro arquivo
     FILE *arq2 = fopen("Poem2.txt", "w");
     if(arq2 == NULL){
         printf("Não foi possível acessar o arquivo!");
         exit(1);
     }
-
-    fputs(texto, arq2);
-    
+    char a = 'a';
+    putc(a, arq2);
+    for(i=0; i<229; i++){
+        c = texto[i];
+        putc(c, arq2);
+        printf("%c", c);
+    }
     fclose(arq2);
 
 

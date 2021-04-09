80b464c474d43997aaf430478833f5d5a77647b1
karenmentges <karen_ruvermentges@hotmail.com>
2021-04-08 22:01:02 -0300

    Changes

--- Manipulação_de_Arquivos_em_C/Atividade_1.c
+++ Manipulação_de_Arquivos_em_C/Atividade_1.c
@@ -4,27 +4,29 @@
 
 int main() {
 
+    char texto[229];
+
     // Leitura do texto
-    FILE *arq = fopen("Poem.txt", "r+");
+    FILE *arq = fopen("Poem.txt", "r");
     if(arq == NULL){
         printf("Não foi possível acessar o arquivo!");
         exit(1);
     }
-
-    char c;
-    char espaco;
-    while(!feof(arq)){
-        c = fgetc(arq);
-        if(c==' '){
-            putc(espaco, arq);
-        }
-        else if(c > 96){
-            c = c - 32;
-        }
-        putc(c, arq);
-    }
+    
+    fgets(texto, 229, arq);
 
     fclose(arq);
+    
+
+    FILE *arq2 = fopen("Poem2.txt", "w");
+    if(arq2 == NULL){
+        printf("Não foi possível acessar o arquivo!");
+        exit(1);
+    }
+
+    fputs(texto, arq2);
+    
+    fclose(arq2);
 
 
     return 0;

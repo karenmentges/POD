bc16567de1d73936dae18ebe9e4ac0e578c70b12
karenmentges <karen_ruvermentges@hotmail.com>
2021-04-09 22:16:23 -0300

    Changes

--- Manipulação_de_Arquivos_em_C/Atividade_3.c
+++ Manipulação_de_Arquivos_em_C/Atividade_3.c
@@ -2,11 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+typedef struct {
+    int matricula;
+    char nome[41];
+    double IAA;
+} Aluno;
+
 int main() {
 
-    int op, matricula[10], total;
-    char nome[41];
-    double *IAA;
+    int op, total;
+    Aluno aluno, alunoLido;
 
     // Adicionando alunos
     FILE *arq = fopen("ListadeAlunos.bin", "wb");
@@ -16,32 +22,27 @@ int main() {
     }
     for(int i=0; i<100; i++){
         printf("1 - Adicionar aluno\n0 - Sair\n");
-        scanf("%d", op);
+        scanf("%d", &op);
         if(op==0){
+            fseek(arq, 0, SEEK_SET); // volta para o inicio
+            while(fread(&alunoLido, sizeof(Aluno), 1, arq)){
+                printf("Matricula: %d\nNome: %s\nIAA: %f", alunoLido.matricula, alunoLido.nome, alunoLido.IAA);
+            }
             break;
         }
         else if(op==1){
             printf("Insira a matricula do aluno: ");
-            scanf("%d", &matricula);
+            scanf("%d", &aluno.matricula);
             printf("Insira o nome do aluno: ");
-            scanf("%s", &nome);
-            printf("Insira o nome do aluno: ");
-            scanf("%f", &IAA);
+            scanf("%s", aluno.nome);
+            printf("Insira o IAA do aluno: ");
+            scanf("%lf", &aluno.IAA);
 
-            total = fwrite(matricula, sizeof(int), 10, arq);
-            if(total != 10)
+            total = fwrite(&aluno, sizeof(Aluno), 1, arq);
+            if(total != 1){
                 printf("Erro na gravação");
             }
-            total = fwrite(nome, sizeof(char), 41, arq);
-            if(total != 41)
-                printf("Erro na gravação");
-            }
-            total = fwrite(IAA, sizeof(double), 1, arq);
-            if(total != 1)
-                printf("Erro na gravação");
         }
-    
     }
-
-    
+    fclose(arq);
 }
